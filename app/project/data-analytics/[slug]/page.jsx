2f775c697440200ec1a05d4aae74ef0777c3b2d7
Muhammad Jundullah <sayidmuhammad15@gmail.com>
2025-05-09 06:36:04 +0700

    update

--- app/project/data-analytics/[slug]/page.jsx
+++ app/project/data-analytics/[slug]/page.jsx
@@ -1,9 +1,7 @@
 "use client";
 
 import React from "react";
-
 import { fetchDataFromAPI } from "@/lib/actions";
-
 import { useEffect, useState } from "react";
 
 export default function Page({ params }) {
@@ -22,11 +20,37 @@ export default function Page({ params }) {
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Project: {slug}</h1>
       {data ? (
-        <pre className="p-4 rounded-md text-sm overflow-x-auto">
-          {JSON.stringify(data, null, 2)}
-        </pre>
+        <div>
+          {data.map((item) => (
+            <div key={item.id} className="mb-8 h-screen">
+              <h2 className="text-xl font-semibold">{item.judul}</h2>
+              <p
+                className="text-gray-600 mb-2"
+                dangerouslySetInnerHTML={{ __html: item.desc }}></p>
+              <p>
+                <strong>Category:</strong> {item.category}
+              </p>
+              <p>
+                <strong>Technologies:</strong> {item.tech}
+              </p>
+              <p>
+                <strong>GitHub URL:</strong>{" "}
+                <a
+                  href={item.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-500 underline">
+                  {item.url}
+                </a>
+              </p>
+              <div className="mt-4">
+                <strong>Dashboard:</strong>
+                <div dangerouslySetInnerHTML={{ __html: item.site }} />
+              </div>
+            </div>
+          ))}
+        </div>
       ) : (
         <div className="flex flex-col justify-center items-center h-32">
           <p className="mb-2">Loading data, please wait...</p>

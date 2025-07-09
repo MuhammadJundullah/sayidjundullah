931c8ad4a1f7af2edbf799344a472f5adeb52f52
Muhammad Jundullah <sayidmuhammad15@gmail.com>
2025-07-09 17:42:48 +0700

    integrasi prisma db update

--- app/api/educations/route.ts
+++ app/api/educations/route.ts
@@ -2,7 +2,7 @@ import prisma from "@/lib/prisma";
 
 export async function GET() {
   try {
-    const educations = await prisma.education.findMany({
+    const educations = await prisma.educations.findMany({
       orderBy: { id: "desc" },
     });
     return new Response(JSON.stringify(educations), {
